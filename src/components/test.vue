e37e33b5a334016a575ceb2a30bab82df3682730
Unknown <kfranck.web01@gmail.com>
2017-03-17 12:21:25 +0100

    Creating my own component and adding some data

---
+++ src/components/test.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>{{title}}</h1>
+    <p><b>Hi,</b> {{user.firstName}} {{user.lastName}}</p>
+    <p v-text = "user.age"> </p>
+  </div>
+</template>
+
+<script>
+export default{
+name: 'test',
+data() {
+    return {
+      title:'Hello world',
+      user: {
+        firstName: 'John',
+        lastName: 'Doe',
+        age: 41
+      }
+    }
+  }
+}
+
+
+</script>
+
+<style scoped>
+
+
+</style>
